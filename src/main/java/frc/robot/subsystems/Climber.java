4a6cd31417103bd1a00ae9e19a62a5e5a2868750
2024davidchen <2024davidchen@gmail.com>
2022-03-19 13:54:18 -0400

    Added climber function

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,17 +8,26 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants.CAN;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.CommandXboxController;
 
 public class Climber extends SubsystemBase {
   private final CANSparkMax climber = new CANSparkMax(CAN.climberID, MotorType.kBrushless);
+  private final CommandXboxController controller = new CommandXboxController(0);
+
   /** Creates a new Climber. */
   public Climber() {
+    
     climber.restoreFactoryDefaults();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    climber.set(controller.getRightTriggerAxis() * 0.5);
     
+  }
+
+  public void pullUp(double speed){
+    climber.set(-1 * speed);
   }
 }
